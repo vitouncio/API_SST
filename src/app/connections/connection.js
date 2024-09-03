54b7e9c7961ebcbd3c070b4636c181f08bd67e7c
Amanda <amandaalmeidadev@gmail.com>
2024-09-03 16:25:04 -0300

    Descrição das mudanças feitas

--- src/app/connections/connection.js
+++ src/app/connections/connection.js
@@ -5,7 +5,7 @@ const conexao = mysql.createConnection({
     host: 'localhost',
     port: '3306',
     user: 'root',
-    password: 'Ronaldovitor10!',
+    password: '28240613asd',
     database: 'bd_sst'
 })
 
